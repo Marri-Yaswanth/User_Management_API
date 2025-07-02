58ddf1762511567e6532988059accb603b71a79d
MarriYaswanth <yashumarri24@gmail.com>
2025-07-02 14:04:49 +0530

    Done with the CRUD Operations in user using springboot

---
+++ src/test/java/com/example/demo/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User>findByEmail(String email);
+    List<User>findByContainingIgnoreCase(String name);
+
+}
