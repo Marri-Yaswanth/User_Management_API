2844acb47d3d8788f331dea00d38b993c6616516
MarriYaswanth <yashumarri24@gmail.com>
2025-07-03 13:26:41 +0530

    hi

--- src/test/java/com/example/demo/Repository/UserRepository.java
+++ src/test/java/com/example/demo/Repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.Repository;
 
 import com.example.demo.Model.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
